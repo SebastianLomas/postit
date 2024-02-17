a116fa3b3cce9663c1439e3d6e4bce81edb9e107
Sebastian Lomas <sebastianlomaslopez@hotmail.com>
2024-02-16 22:11:16 -0600

    Fixing centering body

--- src/App.vue
+++ src/App.vue
@@ -27,8 +27,8 @@ import AppHeaderVue from './components/AppHeader.vue';
   height: 100vh;
   grid-template-columns: repeat(auto-fit, $postItViewSize);
   grid-template-rows: repeat(auto-fit, $postItViewSize);
-  gap: 0.5rem;
-  justify-items: center;
-  align-items: center;
+  gap: 1rem;
+  justify-content: center;
+  margin-top: 1.5rem;
 }
 </style>
