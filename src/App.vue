b3b27a57f4e6faa3560d0f44dc753ccb791dcba8
Sebastian Lomas <sebastianlomaslopez@hotmail.com>
2024-02-16 21:49:53 -0600

    Logo done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import PostItViewVue from './components/PostItView.vue';
+import AppHeaderVue from './components/AppHeader.vue';
 </script>
 
 <template>
+  <AppHeaderVue />
   <PostItViewVue />
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <h1 class="header__logo">
+      POSTiT<span class="header__logo--com">.COM</span>
+    </h1>
+  </header>
+</template>
+
+<style lang="scss">
+@import "../utils.scss";
+
+.header {
+  display: flex;
+  width: 100%;
+  height: 5rem;
+  align-items: center;
+  padding-left: 0.2rem;
+
+  &__logo {
+    font-size: 4rem;
+    font-family: $logoFont;
+    color: $whiteColor;
+
+    &--com {
+      color: $yellowColor;
+    }
+  }
+}
+</style>
