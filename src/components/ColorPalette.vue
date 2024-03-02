3e4babe3e2e72071f93ee51b51aa8e1843532d7a
Sebastian Lomas <sebastianlomaslopez@hotmail.com>
2024-03-01 23:52:29 -0600

    color palette comented

--- src/components/ColorPalette.vue
+++ src/components/ColorPalette.vue
@@ -3,10 +3,14 @@ import { onMounted } from 'vue';
 import { postItColor, BackgroundColors } from '../utils';
 
 const getInitialSelectedColor = function() {
+  // When the post it edit is opened for the first time, applies the border selected color
+  // to the current color. This solved an error that affected the color palette and the selected color.
   const selectedColor : string = postItColor.value;
   let color : HTMLDivElement | null = null;
 
   switch(selectedColor) {
+    // Based on the background color, it removes the same color border
+    // and applies the selected border.
     case BackgroundColors.YELLOW:
       color = document.querySelector(".border-yellow") as HTMLDivElement;
       color.classList.remove("border-yellow");
@@ -34,13 +38,19 @@ const getInitialSelectedColor = function() {
 }
 
 const changeSelectedColor = function(ev : MouseEvent) {
+  // Selects the current color element
   const selection : HTMLDivElement | null = ev.target as HTMLDivElement;
 
+  // if "selection" is not null and it isn't the parent element
   if(selection !== null && selection.classList[0] !== "colorPalette") {
+    // Choices the previous selected color
     const previouseColorSelected : HTMLDivElement = document.querySelector(".border-selected") as HTMLDivElement;
+    // and its color classname
     const selectedColor : string = selection.classList[1];
 
+    // Removes the border selected to the last selected color
     previouseColorSelected.classList.remove("border-selected")
+
 
     switch(previouseColorSelected.classList[1]) {
       case "bg-yellow":
@@ -60,6 +70,7 @@ const changeSelectedColor = function(ev : MouseEvent) {
         break;
     }
 
+    // Changes the value so the new color can be render
     postItColor.value = selectedColor;
     selection.classList.add("border-selected");
   }
